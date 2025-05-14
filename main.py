be3bff9bb96d9a33b266fb438a790b2f03b6d8bf
francisokol <82500975+francisokol@users.noreply.github.com>
2025-05-14 16:05:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1269,4 +1269,4 @@ def adminLogout():
     return redirect('/')
 
 if __name__ == "__main__":
-app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
